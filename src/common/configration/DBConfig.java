ee64c7723b0e7d6bfba2d42c61e434cfcc4709e2
Chenyu Xiao <s3829221@student.rmit.edu.au>
2021-04-24 02:37:23 +1000

    Change database password

--- src/common/configration/DBConfig.java
+++ src/common/configration/DBConfig.java
@@ -4,6 +4,6 @@ public class DBConfig {
 
 	public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/sepm?allowPublicKeyRetrieval=true&useSSL=false";
 	public static final String DB_USERNAME = "root";
-//	public static final String DB_PASSWORD = "root1234";
-	public static final String DB_PASSWORD = "jasongx89O228";
+	public static final String DB_PASSWORD = "root1234";
+//	public static final String DB_PASSWORD = "jasongx89O228";
 }
