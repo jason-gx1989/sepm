07726f4471a5ccd0233f5bd892b9e38f96170257
jason-gx1989 <jason-gx1989@users.noreply.github.com>
2021-04-25 19:06:50 +1000

    aaa

--- src/test/changeLimitTest.java
+++ src/test/changeLimitTest.java
@@ -6,39 +6,45 @@ import addstaff.StaffService;
 import changeLimit.ChangeLimitService;
 import common.junitUtils.StaffTestDao;
 
-
 public class changeLimitTest {
-	
-	private ChangeLimitService changeLimitservice = new ChangeLimitService();
-	private StaffService staffService = new StaffService();
-	private StaffTestDao staffTestDao = new StaffTestDao();
-	private final String name = "Nier";
-	private final double newLimit = 40;
-	
-	
+
+	private ChangeLimitService changeLimitservice;
+	private StaffService staffService;
+	private StaffTestDao staffTestDao;
+
+	String name;
+	Double newLimit;
+
 	@Before
 	public void setUp() {
-		
-		// Insert new staff into database
-		staffService.createStaff(name, null, null, null, null, null);
-		
-		// Retrieve staff from database
-		int id = staffTestDao.getByName(name).getId();
-		
-		// Change work limit
-		changeLimitservice.changeLimit(id, newLimit);
+		changeLimitservice = new ChangeLimitService();
+		staffService = new StaffService();
+		staffTestDao = new StaffTestDao();
+
+		name = "Nier";
+		newLimit = 40d;
+
 	}
-	
-	
+
 	@Test
 	public void testLimit() {
-		
+
+		// Insert new staff into database
+		staffService.createStaff(name, null, null, null, null, null);
+
+		// Retrieve staff from database
+		int id = staffTestDao.getByName(name).getId();
+
+		// Change work limit
+		changeLimitservice.changeLimit(id, newLimit);
+
+		// assert
 		assertEquals(newLimit, staffTestDao.getByName(name).getWorkHourLimit(), 0.1);
 	}
-	
+
 	@After
 	public void tearDown() {
-		
+
 		// Delete test data from database
 		staffTestDao.delete(name);
 	}
